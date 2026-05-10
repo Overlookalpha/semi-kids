ea4949fa26f37112c016231c5d826c288f61acac
Overlookalpha <isaiasfr26@gmail.com>
2026-05-10 19:25:01 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnCrianca = document.querySelector(".kids");
+const btnPais = document.querySelector(".pais");
+
+btnCrianca.addEventListener("click", () => {
+
+  alert("Área da Criança em desenvolvimento 👦");
+
+});
+
+btnPais.addEventListener("click", () => {
+
+  alert("Área dos Pais em desenvolvimento 👨");
+
+});
